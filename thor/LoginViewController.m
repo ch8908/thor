f3cd79af062d627ce968f5459643f401574af0f6
ch8908 <ch8908@gmail.com>
2013-09-16 00:23:28 +0800

    add login view framework

---
+++ thor/LoginViewController.m
@@ -0,0 +1,29 @@
+//
+// Created by Huang ChienShuo on 9/15/13.
+// Copyright (c) 2013 ThousandSquare. All rights reserved.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "LoginViewController.h"
+
+@interface LoginViewController()
+@property (nonatomic) UIButton* logInWithFacebookButton;
+@end
+
+@implementation LoginViewController
+
+- (id) initLogin
+{
+    self = [super initWithNibName:nil bundle:nil];
+    if (self)
+    {
+
+    }
+
+    return self;
+}
+
+
+@end
