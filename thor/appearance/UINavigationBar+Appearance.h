8abe70b934ff19a150dad44331c60e14559def31
Kros <ch8908@gmail.com>
2014-03-02 20:04:45 +0800

    remove unused library

--- thor/appearance/UINavigationBar+Appearance.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Huang ChienShuo on 1/22/14.
-// Copyright (c) 2014 ThousandSquare. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-@interface UINavigationBar(Appearance)
-@end
